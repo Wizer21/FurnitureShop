3818bb8ca0ffc2f86addd25e37de8d13b7b50e0a
Wizer <simon.tessier8@gmail.com>
2021-04-30 18:30:43 +0200

    Baked Scene

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -16,7 +16,11 @@ export default {
     const scene = new THREE.Scene()
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 20)
-    camera.position.set(0, -2, 0) 
+    camera.rotation.x -= Math.PI * 1.5
+    camera.rotation.y += Math.PI
+
+    camera.position.z -= 2
+    camera.position.y += 1
     scene.add(camera)
 
     const light = new THREE.AmbientLight( 0xffffff );
@@ -40,8 +44,30 @@ export default {
       {
         child.material = bakedMaterial
       })
-      scene.add(gltf.scene)
+
+      // Emissives materials 
+      gltf.scene.children.find(child => child.name === "blueTriangle" ).material = new THREE.MeshBasicMaterial({color: 0x0CAAFF}) 
+      gltf.scene.children.find(child => child.name === "greenTriangle" ).material = new THREE.MeshBasicMaterial({color: 0x73F0E0}) 
+      gltf.scene.children.find(child => child.name === "pinkTriangle" ).material = new THREE.MeshBasicMaterial({color: 0xA349A4}) 
+      gltf.scene.children.find(child => child.name === "pinkTriangle" ).material = new THREE.MeshBasicMaterial({color: 0xA349A4}) 
+
+      gltf.scene.children.find(child => child.name === "lightYellow" ).material = new THREE.MeshBasicMaterial({color: 0xFAEE94}) 
+      gltf.scene.children.find(child => child.name === "lightYellow1" ).material = new THREE.MeshBasicMaterial({color: 0xFAEE94})
+
+      gltf.scene.children.find(child => child.name === "lightLamp" ).material = new THREE.MeshBasicMaterial({color: 0xF5F5F5}) 
+
+      // Wall
+      gltf.scene.children.find(child => child.name === "wall" ).material = new THREE.MeshBasicMaterial({color: 0x010101})
+
+      // Glass
+      gltf.scene.children.find(child => child.name === "glass" ).material = new THREE.MeshBasicMaterial({
+        color: 0x010101,
+        transparent: true, 
+        opacity: 0.7
+      })
+
       gltf.scene.scale.set(0.1, 0.1, 0.1)
+      scene.add(gltf.scene)
     })
     
     const control = new OrbitControls(camera, canvas)
